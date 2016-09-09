864868f5827d49fc65ca9d4c76102637784964e3
Rory Hunter <roryhunter2@gmail.com>
2016-09-09 15:07:28 +0100

    Create definitions.json at runtime

--- rabbitmq/0.1.0/dockerentry.sh
+++ rabbitmq/0.1.0/dockerentry.sh
@@ -2,4 +2,21 @@
 
 cp /run.sh /opt/rancher/bin/
 
+cat > /etc/rabbitmq/definitions.json <<EOF
+{
+  "policies": [
+    {
+      "vhost": "/",
+      "name": "ha",
+      "pattern": "",
+      "definition": {
+        "ha-mode": "all",
+        "ha-sync-mode": "automatic",
+        "ha-sync-batch-size": 5
+      }
+    }
+  ]
+}
+EOF
+
 exec /confd $@
